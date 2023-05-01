fe780a7cb29b441da42d1f084a34447cded0899e
takusea <takusea826@gmail.com>
2023-05-01 19:45:41 +0900

    chore: storybook-addon-nextの追加
    "@/*"のパスエイリアスを通すため、storybook-addon-nextを追加した。

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,13 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-addon-next"
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -12,6 +15,17 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    if(!config.resolve) {
+      return config;
+    }
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src/'),
+    };
+    return config;
   },
 };
 export default config;
