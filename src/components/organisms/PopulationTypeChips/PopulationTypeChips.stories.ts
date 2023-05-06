73a388a5eb79b22c7fa35c913f4ebdf2e109e915
takusea <takusea826@gmail.com>
2023-05-06 22:43:20 +0900

    chore: OrganismコンポーネントにStorybookを追加

---
+++ src/components/organisms/PopulationTypeChips/PopulationTypeChips.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import PopulationTypeChips from ".";
+
+type T = typeof PopulationTypeChips;
+
+const meta: Meta<T> = {
+  component: PopulationTypeChips,
+};
+export default meta;
+
+type Story = StoryObj<T>;
+
+export const Default: Story = {
+  args: {
+    currentIndex: 0
+  },
+};
---
+++ src/components/organisms/PrefectureCheckList/PrefectureCheckList.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import PrefectureCheckList from ".";
+
+type T = typeof PrefectureCheckList;
+
+const meta: Meta<T> = {
+  component: PrefectureCheckList,
+};
+export default meta;
+
+type Story = StoryObj<T>;
+
+export const Default: Story = {
+  args: {
+    prefectures: [
+      {
+        name: "a",
+        code: 0
+      },
+      {
+        name: "b",
+        code: 1
+      },
+      {
+        name: "c",
+        code: 2
+      },
+      {
+        name: "d",
+        code: 3
+      }
+    ],
+    checkedIndexes: [0]
+  },
+};
