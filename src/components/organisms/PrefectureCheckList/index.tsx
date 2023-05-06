325222678a7adafc10f913ca1619d244e4acfda3
takusea <takusea826@gmail.com>
2023-05-07 02:22:57 +0900

    refactor: checkedIndexesをPrefectureCheckListで管理するよう変更
    関心の分離のため、checkedIndexesをPrefectureCheckListで管理するよう変更した。

--- src/components/organisms/PrefectureCheckList/index.tsx
+++ src/components/organisms/PrefectureCheckList/index.tsx
@@ -1,20 +1,30 @@
 import styles from "./styles.module.css";
 
+import { useState } from "react";
+
 import { Prefecture } from "@/types/Prefecture";
 
 import CheckBox from "@/components/molecules/CheckBox";
 
 interface PrefectureCheckListProps {
   prefectures: Prefecture[];
-  checkedIndexes: number[];
   onChange: (checked: boolean, prefCode: number) => void;
 }
 
 const PrefectureCheckList = ({
   prefectures,
-  checkedIndexes = [],
   onChange,
 }: PrefectureCheckListProps): JSX.Element => {
+  const [checkedIndexes, setCheckedIndexes] = useState<number[]>([]);
+
+  const toggleCheckedIndexes = async (prefCode: number) => {
+    if (checkedIndexes.includes(prefCode)) {
+      setCheckedIndexes(checkedIndexes.filter((code) => code !== prefCode));
+    } else {
+      setCheckedIndexes([...checkedIndexes, prefCode]);
+    }
+  };
+
   return (
     <ul className={styles.prefectures}>
       {prefectures.map((prefecture) => (
@@ -22,7 +32,9 @@ const PrefectureCheckList = ({
           <CheckBox
             label={prefecture.name}
             checked={checkedIndexes.includes(prefecture.code)}
-            onClick={(checked) => {onChange(checked, prefecture.code)}}
+            onClick={(checked) => {
+              toggleCheckedIndexes(prefecture.code);
+              onChange(checked, prefecture.code)}}
           />
         </li>
       ))}
