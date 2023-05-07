affe73fc6d8ef4120e750e7dad5f6a752ef7fa3c
takusea <takusea826@gmail.com>
2023-05-07 17:29:27 +0900

    fix: /api/prefectureがエラーになる不具合を修正
    handlerの引数に必要なNextApiRequestがなかったために発生した、/api/prefectureがエラーになる不具合を修正した。

--- src/pages/api/prefecture.ts
+++ src/pages/api/prefecture.ts
@@ -1,11 +1,13 @@
-import type { NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 import { fetchPrefectures } from "@/lib/resas";
 import { Prefecture } from "@/types/Prefecture";
 
 export default async function handler(
+  _req: NextApiRequest,
   res: NextApiResponse<Prefecture[]>
 ) {
   const prefectures: Prefecture[] = await fetchPrefectures();
+
   res.status(200).json(prefectures);
 }
